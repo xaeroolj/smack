d0678bc38355d407e1ac4858a21fc874035d8be1
Roman Trekhlebov <xaero_ol@mail.ru>
2017-11-29 03:33:57 +0300

    lesson4

---
+++ Smack/Utillities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Roman Trekhlebov on 29.11.2017.
+//  Copyright © 2017 Roman Trekhlebov. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
