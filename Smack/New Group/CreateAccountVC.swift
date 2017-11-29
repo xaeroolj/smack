3b9fe498dde0a637112c8bcde2479a43ed8aaf44
Roman Trekhlebov <xaero_ol@mail.ru>
2017-11-29 04:09:08 +0300

    lesson 5 done

--- Smack/New Group/CreateAccountVC.swift
+++ Smack/New Group/CreateAccountVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginVC.swift
+//  CreateAccountVC.swift
 //  Smack
 //
 //  Created by Roman Trekhlebov on 29.11.2017.
@@ -8,19 +8,19 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
-
+class CreateAccountVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    
     @IBAction func closePressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        performSegue(withIdentifier: UNWIND, sender: nil)
+        
     }
     
-    @IBAction func createAccountBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
-    }
+    
 }
