69ddf8e1ad4b2ceaf55c37e12e669da43d1e5937
Roman Trekhlebov <xaero_ol@mail.ru>
2017-12-05 11:19:45 +0300

    lesson 12

--- Smack/New Group/CreateAccountVC.swift
+++ Smack/New Group/CreateAccountVC.swift
@@ -16,6 +16,10 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    //Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,6 +27,7 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
         guard let email = emailTxt.text , emailTxt.text != "" else {return }
         guard let pass = passTxt.text, passTxt.text != "" else { return }
         
@@ -30,7 +35,12 @@ class CreateAccountVC: UIViewController {
             if success {
                 AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
                     if success {
-                        print("Logged in User!", AuthService.instance.authToke)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (succes) in
+                            if success {
+                                print(UserDataServices.instance.name, UserDataServices.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
---
+++ Smack/Services/UserDataServices.swift
@@ -0,0 +1,32 @@
+//
+//  UserDataServices.swift
+//  Smack
+//
+//  Created by Roman Trekhlebov on 05.12.2017.
+//  Copyright © 2017 Roman Trekhlebov. All rights reserved.
+//
+
+import Foundation
+
+class UserDataServices {
+    
+    static let instance = UserDataServices()
+    
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserData(id: String, color: String, avatarName: String, email: String, name: String) {
+        self.id = id
+        self.avatarColor = color
+        self.avatarName = avatarName
+        self.email = email
+        self.name = name
+    }
+    func setAvatarName(avatarName: String) {
+        self.avatarName = avatarName
+    }
+    
+}
